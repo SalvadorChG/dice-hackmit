d3aed7b1eefe81e3cbfbef16225f2c52e6540171
Sebastian Torrealba <sebastiantorrealba@pop-os.localdomain>
2022-10-01 18:19:32 -0300

    updating

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,32 +1,13 @@
 from flask import Flask, jsonify
+import database
 
 app = Flask(__name__)
 
-# we can avoid using mongo if we use this 
-# https://www.geeksforgeeks.org/read-json-file-using-python/
-
-@app.route("/user/<id>")
-def userData(id):
-    id = str(id).strip()
-    return jsonify({
-        "username": "john.doe",
-        "name": "John Doe",
-        "pfp": "http://example.com",
-	    "activity_log": [],
-	    "wish_list": []
-    })
+@app.route("/user/<username>")
+def userData(username):
+    username = username.strip()
+    return jsonify(database.getProfileByUsername(username))
     
 @app.route("/popularactivities")
 def popularActivities():
-
-    return jsonify({ 
-        "id": 123123,
-        "name": "Activity name",
-        "photo": "http://example.com",
-        "location":  "string",
-        "location_link": "string",
-        "category": "Fun",
-        "description": "Sample string",
-        "rating": 6,
-        "sustainable": True
-    })
+    return jsonify(database.getAllActivities())
